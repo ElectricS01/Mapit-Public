0b098869af101f150333564b2a2d3c88b94b9844
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2024-04-26 16:43:23 +1000

    0.8.1

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,7 +14,7 @@
         and
         <a href="https://github.com/Thomas123414">Thomas123414</a>
       </p>
-      <p class="small">Version: 0.8.0</p>
+      <p class="small">Version: 0.8.1</p>
     </div>
   </div>
 </template>
